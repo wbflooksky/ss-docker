165968773c2af1ce3733034426d8c2972a60d5b4
wb-cyh578854 <wb-cyh578854@alibaba-inc.com>
2019-10-11 15:19:44 +0800

    fix:修改ss设置

--- README.md
+++ README.md
@@ -3,10 +3,11 @@
 ## 介绍
 
 基础镜像使用:https://hub.docker.com/r/mritd/shadowsocks/ 后续考虑使用官方自建
+ss 参数：https://github.com/shadowsocks/shadowsocks-libev
 
 ## 构建&使用：
 参考scripts
 
 ## more：
-
+docker pull
 
--- scripts/run.docker.sh
+++ scripts/run.docker.sh
@@ -8,6 +8,6 @@ fi
 
 set -x
 
-docker run -dt -p 6443:6443 adolfheir/shadowsocks:$1 -s "-s 0.0.0.0 -p 6443 -m chacha20-ietf-poly1305 -k test123 -x"
+docker run -dt -p 6443:6443 -p 6500:6500/udp adolfheir/shadowsocks:$1 -m "ss-server" -s "-s 0.0.0.0 -p 6443 -m chacha20-ietf-poly1305 -k admin" -x -e "kcpserver" -k "-t 127.0.0.1:6443 -l :6500 -mode fast2"
 
 set +x
