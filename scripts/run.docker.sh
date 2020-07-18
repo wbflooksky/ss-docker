1a5832b463a4575624392ea9eff43b71ac8b5b47
wbflooksky <52812629+wbflooksky@users.noreply.github.com>
2020-07-18 12:14:35 +0800

    Update run.docker.sh

--- scripts/run.docker.sh
+++ scripts/run.docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+systemctl enable docker.service
 if [ $# -lt 1 ]; then
     echo "Usage:"
     echo "./run_docker.sh tag"
@@ -8,6 +8,6 @@ fi
 
 set -x
 
-docker run -dt -p 6443:6443 -p 6500:6500/udp adolfheir/shadowsocks:$1 -m "ss-server" -s "-s 0.0.0.0 -p 6443 -m chacha20-ietf-poly1305 -k admin" -x -e "kcpserver" -k "-t 127.0.0.1:6443 -l :6500 -mode fast2"
+docker run --restart=always -dt -p 6443:6443 -p 6500:6500/udp adolfheir/shadowsocks:$1 -m "ss-server" -s "-s 0.0.0.0 -p 6443 -m chacha20-ietf-poly1305 -k admin" -x -e "kcpserver" -k "-t 127.0.0.1:6443 -l :6500 -mode fast2"
 
 set +x
